c359ae5787e8633c8a71b22d27afc461ef52d389
Kunal <kunalattrade@gmail.com>
2024-05-22 20:54:00 +0530

    added revision

---
+++ week-1/offline-class-1/Revision.js
@@ -0,0 +1,22 @@
+// Loops revison
+// let ans = 0;
+// for (let i=1; i<=100; i++){
+//      ans += i
+    
+// }
+
+// console.log(ans)
+
+// functions Revison/ Calbacks
+
+function squar(a){
+    return a * a
+}
+
+function sumOfSomething(a,b, fn){
+    const val1 = fn(a)
+    const val2 = fn(b)
+    return val1 + val2
+}
+
+console.log(sumOfSomething(5,8, squar))
